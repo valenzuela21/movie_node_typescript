b79c4b0a68b1908b584aee5fcfd4bfc6a7f920e9
David Valenzuela Pardo <vlzdavid12@outlook.com>
2023-05-11 15:15:22 -0500

    Node  Typescript

--- src/controllers/comment.controller.ts
+++ src/controllers/comment.controller.ts
@@ -1,8 +1,17 @@
 import {Request, Response} from "express";
 import {IComment, Comment} from "../models/comments.model";
+import {Movie} from "../models/movie.model";
 
 export const addComment = async (req: Request | any, res: Response) => {
     const {comment, state, movie} = req.body;
+
+    const movieDB = await Movie.findById({_id: movie});
+
+    if(!movieDB){
+        return res.status(200).json({
+           msg: "Error no existe esta movie en la base de datos"
+        });
+    }
 
     const data: IComment = {
         comment,
