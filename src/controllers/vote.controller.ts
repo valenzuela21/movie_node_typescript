34fa7f6479e1b49e5af398cdf296df42f3c25784
David Valenzuela Pardo <vlzdavid12@outlook.com>
2023-05-12 16:12:51 -0500

    Vote Node js

--- src/controllers/vote.controller.ts
+++ src/controllers/vote.controller.ts
@@ -1,21 +1,32 @@
 import {Request, Response} from "express";
 import {IVote, Vote} from "../models/vote.model";
 
-export const listVoteMovie  =  async (req: Request, res: Response) => {
+export const listVoteMovie = async (req: Request, res: Response) => {
     const pageNumber: number = req.body.offset || 1;
     const nPerPage: number = req.body.perpage || 12;
     const votes = await Vote.find()
-        .sort( { _id: 1 } )
-        .skip( pageNumber > 0 ? ( ( pageNumber - 1 ) * nPerPage ) : 0 )
-        .limit( nPerPage );
+        .populate("movie")
+        .populate("user", ["name", "email"])
+        .sort({_id: 1})
+        .skip(pageNumber > 0 ? ((pageNumber - 1) * nPerPage) : 0)
+        .limit(nPerPage);
     return res.status(201).json(votes);
 };
 
-export const addVoteMovie  =  async (req: Request, res: Response) => {
-   const {score, user, movie} = req.body;
+export const addVoteMovie = async (req: Request | any, res: Response) => {
+    const {score, movie} = req.body;
+    const user_id = req.user._id;
+    const existScore = await Vote.findOne({user: user_id, movie: movie});
+    if(existScore){
+        res.status(401).json({
+            msg: "No puedes votar..."
+        });
+    }
+
+
     const data: IVote = {
         score,
-        user,
+        user: user_id,
         movie
     };
 
--- src/routes/vote.route.ts
+++ src/routes/vote.route.ts
@@ -2,12 +2,13 @@ import {Router} from "express";
 import * as VoteController from "../controllers/vote.controller";
 import {body} from "express-validator";
 import {validateInputs} from "../middlewares/validate-inputs";
+import {validateJwt} from "../helpers/validateJwt.help";
 const router: Router = Router();
 
 router.get("/", [], VoteController.listVoteMovie);
 router.post("/", [
+    validateJwt,
     body("score", "Ingrese el campo score no puede estar vacio").not().isEmpty().isInt({ min: 0, max: 5 }).withMessage("El campo score es númerico de 0 a 5"),
-    body("user", "Ingrese el campo user el ID USER").not().isEmpty().isMongoId().withMessage("Ingrese la ID User"),
     body("movie", "Ingrese el campo movie el ID MOVIE").not().isEmpty().isMongoId().withMessage("Ingrese la ID Movie"),
     validateInputs
 ], VoteController.addVoteMovie);
