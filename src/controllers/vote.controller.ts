59c8f1352f032be9b26343a3562c855d2abdf37b
David Valenzuela Pardo <vlzdavid12@outlook.com>
2023-05-12 15:57:13 -0500

    Vote all node

--- src/controllers/vote.controller.ts
+++ src/controllers/vote.controller.ts
@@ -1,11 +1,14 @@
 import {Request, Response} from "express";
 import {IVote, Vote} from "../models/vote.model";
-import {ObjectId} from "mongodb";
 
 export const listVoteMovie  =  async (req: Request, res: Response) => {
-        res.json({
-            msg: "prueba..."
-        });
+    const pageNumber: number = req.body.offset || 1;
+    const nPerPage: number = req.body.perpage || 12;
+    const votes = await Vote.find()
+        .sort( { _id: 1 } )
+        .skip( pageNumber > 0 ? ( ( pageNumber - 1 ) * nPerPage ) : 0 )
+        .limit( nPerPage );
+    return res.status(201).json(votes);
 };
 
 export const addVoteMovie  =  async (req: Request, res: Response) => {
