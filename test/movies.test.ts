e9098fcfc799b74c8fdcd6298eb25ca275dc2eb5
David Valenzuela Pardo <vlzdavid12@outlook.com>
2023-05-12 23:57:01 -0500

    Update Testing

--- test/movies.test.ts
+++ test/movies.test.ts
@@ -1,9 +1,8 @@
 import request from "supertest";
 import app from "../src/app";
-import {describe, it} from "node:test";
 
 describe("GET /api", () => {
-    it("should return 200 OK", () => {
+    it("should return 201 OK", () => {
         return request(app).get("/api/movie/list")
             .expect(201);
     });
